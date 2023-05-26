9c3a057a40b8c6f72e0299433223ccbe5471342c
Anthony Ruffalo <antruffii@gmail.com>
2023-05-25 22:47:37 -0500

    LastWatch variant implemented. Incap passive needs testing

---
+++ EdgesMod/Eliza/LastWatchElizaCharacterCardController.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Handelabra;
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+
+
+namespace EdgesOfTheMultiverse.Eliza
+{
+	public class LastWatchElizaCharacterCardController : HeroCharacterCardController
+	{
+		public LastWatchElizaCharacterCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
+		{ }
+			
+		public override IEnumerator UsePower(int index = 0)
+		{
+			// Destroy an Runic Rapier.
+
+			List<Card> list = FindCardsWhere((Card c) => c.Identifier == "RunicRapier" && c.IsInPlay).ToList();
+			IEnumerator e;
+			if (list.Count > 0)
+			{
+				//If you do, destroy a ongoing or non-character device
+				List<SelectCardDecision> storedResults = new List<SelectCardDecision>();
+				IEnumerator e2 = base.GameController.SelectCardAndStoreResults(base.DecisionMaker, SelectionType.DestroyCard, list, storedResults);
+				if (base.UseUnityCoroutines)
+				{
+					yield return base.GameController.StartCoroutine(e2);
+				} else
+				{
+					base.GameController.ExhaustCoroutine(e2);
+				}
+
+				SelectCardDecision selectCardDecision = storedResults.FirstOrDefault();
+				if (selectCardDecision != null && selectCardDecision.SelectedCard != null)
+				{
+					IEnumerator e3 = base.GameController.DestroyCard(base.DecisionMaker, selectCardDecision.SelectedCard, optional: false, cardSource: base.GetCardSource());
+					if (base.UseUnityCoroutines)
+					{
+						yield return base.GameController.StartCoroutine(e3);
+					} else
+					{
+						base.GameController.ExhaustCoroutine(e3);
+					}
+					IEnumerator e4 = base.GameController.SelectAndDestroyCard(base.DecisionMaker, new LinqCardCriteria((Card c) => IsOngoing(c) || (c.IsDevice && !c.IsCharacter)), optional: false, cardSource: base.GetCardSource());
+					if (base.UseUnityCoroutines)
+					{
+						yield return base.GameController.StartCoroutine(e4);
+					} else
+					{
+						base.GameController.ExhaustCoroutine(e4);
+					}
+					IEnumerator e5 = base.GameController.DrawCard(this.HeroTurnTaker, cardSource: base.GetCardSource());
+					if (base.UseUnityCoroutines)
+					{
+						yield return base.GameController.StartCoroutine(e5);
+					} else
+					{
+						base.GameController.ExhaustCoroutine(e5);
+					}
+				}
+			} else
+			{
+				//Otherwise, search your deck for a Runic Rapier and put it into play.
+				e = SearchForCards(this.DecisionMaker, true, false, 1, 1, new LinqCardCriteria((Card c) => c.Identifier == "RunicRapier"), true, false, false);
+				if (base.UseUnityCoroutines)
+				{
+					yield return base.GameController.StartCoroutine(e);
+				}
+				else
+				{
+					base.GameController.ExhaustCoroutine(e);
+				}
+			}
+		}
+		public override IEnumerator UseIncapacitatedAbility(int index)
+		{
+			IEnumerator e = null;
+
+			switch (index)
+			{
+				case 0:
+					{
+						//"Destroy an Ongoing or Enviroment card."
+						e = base.GameController.SelectAndDestroyCard(this.HeroTurnTakerController, new LinqCardCriteria((Card c) => IsOngoing(c) || c.IsEnvironment, "ongoing or enviroment"), false, cardSource: base.GetCardSource());
+						if (base.UseUnityCoroutines)
+						{
+							yield return base.GameController.StartCoroutine(e);
+						}
+						else
+						{
+							base.GameController.ExhaustCoroutine(e);
+						}
+						break;
+					}
+				case 1:
+					{
+						e = SelectHeroToPlayCard(base.DecisionMaker);
+						if (base.UseUnityCoroutines)
+						{
+							yield return base.GameController.StartCoroutine(e);
+						} else
+						{
+							base.GameController.ExhaustCoroutine(e);
+						}
+						break;
+					}
+			}
+			yield break;
+		}
+	
+		public override void AddSideTriggers()
+		{
+			if (base.Card.IsFlipped)
+			{
+				AddSideTrigger(AddTrigger((DealDamageAction da) => da.Target.IsHeroCharacterCard && da.TargetHitPointsAfterBeingDealtDamage <= 0, PrevantDamageResponse, new TriggerType[3]
+				{
+					TriggerType.WouldBeDealtDamage,
+					TriggerType.CancelAction,
+					TriggerType.RemoveFromGame
+				}, TriggerTiming.Before));
+			}
+		}
+
+		public override IEnumerator AfterFlipCardImmediateResponse()
+		{
+			RemoveSideTriggers();
+			AddSideTriggers();
+			yield return null;
+		}
+
+
+		private IEnumerator PrevantDamageResponse(DealDamageAction da)
+		{
+
+			List<YesNoCardDecision> storedResults = new List<YesNoCardDecision>();
+			IEnumerator e0 = base.GameController.MakeYesNoCardDecision(base.HeroTurnTakerController, SelectionType.PreventDamage, da.Target, da, storedResults, cardSource: base.GetCardSource());
+			if (base.UseUnityCoroutines)
+			{
+				yield return base.GameController.StartCoroutine(e0);
+			}
+			else
+			{
+				base.GameController.ExhaustCoroutine(e0);
+			}
+
+			if (DidPlayerAnswerYes(storedResults))
+			{
+
+				IEnumerator e = CancelAction(da, showOutput: false, cancelFutureRelatedDecisions: false);
+				IEnumerator e2 = base.GameController.SendMessageAction(base.Card.Title + " prevants " + da.DamageSource.TitleOrName + " from dealing damage!", Priority.Low, GetCardSource(), null, showCardSource: true);
+				if (base.UseUnityCoroutines)
+				{
+					yield return base.GameController.StartCoroutine(e);
+					yield return base.GameController.StartCoroutine(e2);
+				}
+				else
+				{
+					base.GameController.ExhaustCoroutine(e);
+					base.GameController.ExhaustCoroutine(e2);
+				}
+
+				e = base.GameController.MoveCard(base.TurnTakerController, base.Card, base.TurnTaker.OutOfGame, cardSource: base.GetCardSource());
+				e2 = base.GameController.SendMessageAction(base.Card.Title + "is removed from the game.", Priority.Medium, GetCardSource(), null, showCardSource: true);
+
+				if (base.UseUnityCoroutines)
+				{
+					yield return base.GameController.StartCoroutine(e);
+					yield return base.GameController.StartCoroutine(e2);
+				}
+				else
+				{
+					base.GameController.ExhaustCoroutine(e);
+					base.GameController.ExhaustCoroutine(e2);
+				}
+			}
+		}
+	}
+}
---
+++ EdgesModTest/ElizaVariantTest.cs
@@ -0,0 +1,161 @@
+﻿using NUnit.Framework;
+using System;
+using EdgesOfTheMultiverse;
+using EdgesOfTheMultiverse.Eliza;
+using Handelabra;
+using Handelabra.Sentinels.Engine.Model;
+using Handelabra.Sentinels.Engine.Controller;
+using System.Linq;
+using System.Collections;
+using Handelabra.Sentinels.UnitTest;
+using System.Collections.Generic;
+
+namespace EdgesModTest
+{
+	[TestFixture()]
+
+	public class ElizaVariantTest : BaseTest
+	{
+		protected HeroTurnTakerController eliza { get { return FindHero("Eliza"); } }
+
+		#region HelperFunctions
+		protected string HeroNamespace = "EdgesOfTheMultiverse.Eliza";
+
+		private void SetupIncap(TurnTakerController villain)
+		{
+			SetHitPoints(eliza, 1);
+			DealDamage(villain, eliza, 2, DamageType.Melee);
+		}
+
+		#endregion
+
+		[Test()]
+		public void TestLastWatchLoads()
+		{
+			SetupGameController("BaronBlade", HeroNamespace + "/LastWatchElizaCharacter", "Megalopolis");
+			Assert.AreEqual(3, this.GameController.TurnTakerControllers.Count());
+
+			Assert.IsNotNull(eliza);
+			Assert.IsInstanceOf(typeof(LastWatchElizaCharacterCardController), eliza.CharacterCardController);
+
+			Assert.AreEqual(22, eliza.CharacterCard.HitPoints);
+		}
+
+		[Test()]
+		public void TestLastWatchPowerDestroyOnlyCopyDevice()
+		{
+			SetupGameController("BaronBlade", HeroNamespace + "/LastWatchElizaCharacter", "Megalopolis");
+			
+			StartGame();
+
+			var rr = PutIntoPlay("RunicRapier");
+			var mdp = GetCardInPlay("MobileDefensePlatform");
+
+			DecisionDestroyCard = mdp;
+			QuickHandStorage(eliza.ToHero());
+			UsePower(eliza.CharacterCard);
+			QuickHandCheck(1);
+			AssertInTrash(eliza, rr);
+			AssertInTrash(baron, mdp);
+		}
+
+		[Test()]
+		public void TestLastWatchPowerDestroyMulticopyOngoing()
+		{
+			SetupGameController("BaronBlade", HeroNamespace + "/LastWatchElizaCharacter", "Megalopolis");
+
+			StartGame();
+
+			PutIntoPlay("ArcaneArm");
+			PutIntoPlay("TelekineticCell");
+
+			var rr = PutIntoPlay("RunicRapier");
+			var rr2 = PutIntoPlay("RunicRapier");
+			var lff = PutIntoPlay("LivingForceField");
+
+			DecisionSelectCards = new List<Card> { rr, lff };
+			QuickHandStorage(eliza.ToHero());
+			UsePower(eliza.CharacterCard);
+			QuickHandCheck(1);
+			AssertInTrash(eliza, rr);
+			AssertInTrash(baron, lff);
+			AssertIsInPlay(rr2);
+		}
+
+		[Test()]
+		public void TestLastWatchPowerNoRapiersSearch()
+		{
+			SetupGameController("BaronBlade", HeroNamespace + "/LastWatchElizaCharacter", "Megalopolis");
+
+			StartGame();
+
+			var rr = PutOnDeck("RunicRapier");
+
+			DecisionSelectCard = rr;
+			UsePower(eliza.CharacterCard);
+			AssertIsInPlay(rr);
+		}
+
+		[Test()]
+		public void TestLastWatchIncap1()
+		{
+			SetupGameController("BaronBlade", HeroNamespace + "/LastWatchElizaCharacter", "Legacy", "Megalopolis");
+
+			StartGame();
+			SetupIncap(baron);
+
+			AssertIncapacitated(eliza);
+
+			var lff = PutIntoPlay("LivingForceField");
+			var tp = PutIntoPlay("TrafficPileup");
+
+			AssertNextDecisionChoices(new Card[] { lff, tp });
+			DecisionSelectCard = lff;
+			UseIncapacitatedAbility(eliza, 0);
+			AssertInTrash(lff);
+			AssertIsInPlay(tp);
+		}
+
+		[Test()]
+		public void TestLastWatchIncap2()
+		{
+			SetupGameController("BaronBlade", HeroNamespace + "/LastWatchElizaCharacter", "Legacy", "Megalopolis");
+
+			StartGame();
+			SetupIncap(baron);
+
+			AssertIncapacitated(eliza);
+
+			var ip = PutInHand(legacy, "InspiringPresence");
+
+			DecisionSelectTurnTaker = legacy.TurnTaker;
+			DecisionSelectCardToPlay = ip;
+
+			UseIncapacitatedAbility(eliza, 1);
+			AssertIsInPlay(ip);
+		}
+
+		[Test()]
+		public void TestLastWatchResponseYes()
+		{
+			SetupGameController("BaronBlade", HeroNamespace + "/LastWatchElizaCharacter", "Legacy", "Megalopolis");
+
+			StartGame();
+			SetupIncap(baron);
+
+			AssertIncapacitated(eliza);
+
+			SetHitPoints(legacy.CharacterCard, 2);
+
+			DecisionYesNo = true;
+			QuickHPStorage(legacy.CharacterCard);
+
+			PutOnDeck("LivingForceField");
+			PlayCard(baron, "HastenDoom");
+
+			QuickHPCheck(0);
+			AssertIsInPlay(legacy.CharacterCard);
+			AssertOutOfGame(eliza.CharacterCard);
+		}
+	}
+}
